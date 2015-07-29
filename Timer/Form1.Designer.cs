4fe9357bfe2a5fa2c42febe1eebd357ff61b381a
reuben <reuben@altoqi.com.br>
2015-07-29 08:31:32 -0300

    first

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Timer
+{
+    partial class Timer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.nudHour = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudMinutes = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbTime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(33, 142);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(80, 67);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(119, 142);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 67);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // nudHour
+            // 
+            this.nudHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudHour.Location = new System.Drawing.Point(79, 11);
+            this.nudHour.MaximumSize = new System.Drawing.Size(150, 0);
+            this.nudHour.Name = "nudHour";
+            this.nudHour.Size = new System.Drawing.Size(120, 45);
+            this.nudHour.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Horas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Minutos:";
+            // 
+            // nudMinutes
+            // 
+            this.nudMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudMinutes.Location = new System.Drawing.Point(79, 71);
+            this.nudMinutes.Name = "nudMinutes";
+            this.nudMinutes.Size = new System.Drawing.Size(120, 45);
+            this.nudMinutes.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(33, 227);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(166, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(98, 262);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(0, 13);
+            this.lbTime.TabIndex = 7;
+            // 
+            // Timer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 295);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.nudMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudHour);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Timer";
+            this.Text = "Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.nudHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown nudHour;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudMinutes;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbTime;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+namespace Timer
+{
+    public partial class Timer : Form
+    {
+        static int timeRemaing;
+        static int labelTime;
+        public Timer()
+        {
+            InitializeComponent();    
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if ((nudMinutes.Value > 0) || (nudHour.Value > 0))
+            {
+                Decimal timeInterval = GetTimeBySeconds();
+                timeRemaing = (int)timeInterval;                               
+                timer1.Interval = 1000;
+                timer1.Tick += new EventHandler(Timer_Tick);
+                progressBar1.Maximum = timeRemaing;
+                timer1.Start();
+                labelTime = timeRemaing;
+                SetComponentsVisible(false);
+            }
+        }
+
+        Decimal GetTimeBySeconds()
+        {
+            Decimal hour = nudHour.Value * 60 * 60;
+            Decimal minutes = nudMinutes.Value * 60;            
+            return hour + minutes;
+        }
+
+        void SetComponentsVisible(bool value)
+        {
+            nudHour.Enabled = value;
+            nudMinutes.Enabled = value;
+            btnStart.Enabled = value;
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            SetComponentsVisible(true);
+            progressBar1.Value = 0;
+            timer1.Stop();
+        }
+
+        String SetTimeToLabel(int time)
+        {
+            TimeSpan timespan = TimeSpan.FromSeconds(time);
+
+            //here backslash is must to tell that colon is
+            //not the part of format, it just a character that we want in output
+            return timespan.ToString(@"hh\:mm\:ss\:fff");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timeRemaing > progressBar1.Value)
+            {
+                progressBar1.Value++;                
+                lbTime.Text = SetTimeToLabel(labelTime--);                
+            }
+            else
+            {
+                timer1.Stop();
+                Process.Start("shutdown", "/s /t 0");
+            }
+        }
+    }
+}
